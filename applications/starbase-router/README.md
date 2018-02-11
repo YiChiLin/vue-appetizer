d05de609ec6d74039d270336483d17b86b7c651b
jean.lin52 <jean.lin52@TI03BY32>
2018-02-11 16:20:16 +0800

    intergrate firebase

--- applications/starbase-router/README.md
+++ applications/starbase-router/README.md
@@ -25,3 +25,8 @@
     // Use this.$router/this.$route to injecting router
     this.$route.params.type
 ```
+
+### Mode
+- Default mode is 'hash'. Url will have '#' which is ugly. 
+- The another mode 'history'. It is 'HTML history mode' which can get rid of '#' and use API 'pushState' & 'replaceState' to management URL history. 
+- Both mode can achieve that when user hit endpoint in url, single page won't be refresh. Instead it will get page content through ajax. 
--- applications/turnout/src/main.js
+++ applications/turnout/src/main.js
@@ -1,10 +1,30 @@
 import Vue from 'Vue'
 import App from './components/App.vue'
 import VueRouter from 'vue-router'
+import { firebaseApp } from './firebaseApp'
 
 Vue.use(VueRouter)
 
+import Dashboard from './components/Dashboard.vue'
+import Signin from './components/Signin.vue'
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '/dashboard',component: Dashboard},
+    {path: '/signin',component: Signin}    
+  ]
+})
+
+firebaseApp.auth().onAuthStateChanged(user=>{
+  if(user)
+    router.push('/dashboard')
+  else
+    router.replace('/signin')
+})
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
