ce607aa91cd8f9f1129c8649c28d6e65583d78e2
jean.lin52 <jean.lin52@TI03BY32>
2018-02-16 12:45:23 +0800

    Recreated project

---
+++ applications/turnout/src/components/Dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="">
+        Dashboard Component
+        {{$store.state}}
+    </div>
+</template></template>
---
+++ applications/turnout/src/components/Signin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="">
+    <h3>Sign In</h3>
+    <div class="form-group">
+      <input type="text" placeholder="email" id="form-control" v-model="email">
+      <input type="password" placeholder="password" id="form-control" v-model="password">
+      <button class="btn btn-primary">Sign In</button>
+      <br>
+      <br>
+      <p>{{error.message}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {
+    firebaseApp
+  } from '../firebaseApp'
+
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+        error: {
+          message: ''
+        }
+      }
+    },
+    methods: {
+      signIn() {
+        firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+        .catch(error=>{
+            this.error = error;
+        })
+      }
+    }
+  }
+
+</script>
